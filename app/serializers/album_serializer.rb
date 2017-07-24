fa7bbbf1ec40853080325f17161029de57bbb9a8
Shane Barringer <shane.barringer@outlook.com>
2017-07-24 14:39:40 -0400

    updates album serializer to do/end syntax

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,7 +1,9 @@
 class AlbumSerializer < ActiveModel::Serializer
   attributes :id, :title, :year
 
-  belongs_to :artist { object.artist.name }
+  belongs_to :artist do
+    object.artist.name
+  end
   has_many :songs
 
 end
