0f874c20f9e037c5259d4dbda23d324a64cb5e6f
Shane Barringer <shane.barringer@outlook.com>
2017-07-24 15:41:45 -0400

    adds root route (temporarily directs to artists index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   resources :albums, only: [:index, :show]
   resources :artists, only: [:index, :show] do
     resources :albums, only: [:index, :show] do
-      resources :songs, only: [:index, :show] 
+      resources :songs, only: [:index, :show]
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'artists#index'
+  
 end
